412dc19ac4c493f4135e434a962d8b02cdfc951a
unknown <piotr.benedyk@piotrskarga.pl>
2022-08-02 11:19:14 +0200

    Mail template change

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -1,7 +1,7 @@
 <div style="width:100%; max-width:650px; min-height:300px; margin:auto">
-    <div style="width:100%; height:50px; text-align: center; background: #41ad49">Sempai Mailer</div>
+    <div style="width:100%; padding:20px; text-align: center; background: #41ad49; color:#ffffff">Sempai Mailer</div>
     <div style="width:100%; margin: 40px auto">
         {!! $msg->content !!}
     </div>
-    <div style="width:100%; height:50px; background: #41ad49"></div>
+    <div style="width:100%; padding:20px; min-height: 40px; background: #41ad49"></div>
 </div>
